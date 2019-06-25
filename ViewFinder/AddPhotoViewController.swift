fb2c6087946904736d8ba029aea63af4d4ed32b7
zozo1013 <zoepunjwani@gmail.com>
2019-06-25 11:34:19 -0400

    Iteration 1

---
+++ ViewFinder/AddPhotoViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AddPhotoViewController.swift
+//  ViewFinder
+//
+//  Created by Apple on 6/25/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var newScreen = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newScreen.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func CameraButton(_ sender: UIButton) {
+        newScreen.sourceType = .camera
+        present(newScreen, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var TextField: UITextField!
+    
+    
+    @IBAction func SaveButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func AlbumsButton(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var PlaceHolderPhoto: UIImageView!
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            PlaceHolderPhoto.image = selectedImage
+    }
+        newScreen.dismiss(animated: true, completion: nil)
+    
+    
+    }
+}
